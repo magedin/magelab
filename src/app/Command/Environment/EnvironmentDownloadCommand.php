1129fd3b38ada9ddc0ef30430a1182ff38ba9400
Tiago Sampaio <tiago@tiagosampaio.com>
2021-12-22 23:07:05 -0300

    Improvements.

--- src/app/Command/Environment/EnvironmentDownloadCommand.php
+++ src/app/Command/Environment/EnvironmentDownloadCommand.php
@@ -6,8 +6,10 @@ namespace MageLab\Command\Environment;
 
 use MageLab\Config\Github\MagentoDockerlabRepo;
 use Symfony\Component\Console\Command\Command;
-use Symfony\Component\Console\Input\InputArgument;
+use Symfony\Component\Console\Exception\InvalidOptionException;
+use Symfony\Component\Console\Exception\RuntimeException;
 use Symfony\Component\Console\Input\InputInterface;
+use Symfony\Component\Console\Input\InputOption;
 use Symfony\Component\Console\Output\OutputInterface;
 use Symfony\Component\Process\Exception\ProcessFailedException;
 use Symfony\Component\Process\Process;
@@ -15,20 +17,23 @@ use Symfony\Component\Process\Process;
 class EnvironmentDownloadCommand extends Command
 {
     const ARG_BRANCH = 'branch';
+    const ARG_BRANCH_VALUE = 'master';
     const ARG_DESTINATION = 'destination';
 
     protected function configure()
     {
-        $this->addArgument(
+        $this->addOption(
             self::ARG_BRANCH,
-            InputArgument::OPTIONAL,
+            'b',
+            InputOption::VALUE_OPTIONAL,
             "The branch you'd like to clone.",
-            'develop'
+            self::ARG_BRANCH_VALUE
         );
 
-        $this->addArgument(
+        $this->addOption(
             self::ARG_DESTINATION,
-            InputArgument::OPTIONAL,
+            'd',
+            InputOption::VALUE_OPTIONAL,
             "Where you want to clone the project.",
             '.'
         );
@@ -41,16 +46,19 @@ class EnvironmentDownloadCommand extends Command
      */
     protected function execute(InputInterface $input, OutputInterface $output): int
     {
-        $branchName = $input->getArgument(self::ARG_BRANCH);
-        $destinationPath = $input->getArgument(self::ARG_DESTINATION);
-        $repoSshUrl = MagentoDockerlabRepo::getRepoSshUrl();
+        $branch = $input->getOption(self::ARG_BRANCH);
+        $destinationPath = $input->getOption(self::ARG_DESTINATION);
         $realPath = realpath($destinationPath);
 
         if (!is_writeable($realPath)) {
-            $output->writeln("The destination provided is not writeable. Please check and run the comment again.");
+            throw new RuntimeException("The destination provided is not writeable. Please check and run the comment again.");
         }
 
-        $process = new Process(['git', 'clone', '--branch', $branchName, $repoSshUrl, $destinationPath]);
+        if (!$this->branchExists($branch)) {
+            throw new InvalidOptionException("The branch '{$branch}' to clone does not exist.");
+        }
+
+        $process = new Process(['git', 'clone', '--branch', $branch, $this->getRepoSshUrl(), $destinationPath]);
         $process->run();
 
         if (!$process->isSuccessful()) {
@@ -58,7 +66,39 @@ class EnvironmentDownloadCommand extends Command
         }
 
         $output->writeln("Your project was cloned to the following directory: {$realPath}");
-        $output->writeln("Using the branch: {$branchName}");
+        $output->writeln("Using the branch: {$branch}");
         return Command::SUCCESS;
+    }
+
+    /**
+     * @param string $branch
+     * @return bool
+     */
+    private function branchExists(string $branch): bool
+    {
+        if ($branch === self::ARG_BRANCH_VALUE) {
+            return true;
+        }
+
+        $process = new Process(['git', 'ls-remote', '--heads', $this->getRepoSshUrl(), $branch]);
+        $process->run();
+
+        if (!$process->isSuccessful()) {
+            return false;
+        }
+
+        $output = $process->getOutput();
+        if (empty($output)) {
+            return false;
+        }
+        return true;
+    }
+
+    /**
+     * @return string
+     */
+    private function getRepoSshUrl(): string
+    {
+        return MagentoDockerlabRepo::getRepoSshUrl();
     }
 }
